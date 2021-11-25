aff89aabf5fbca6cb13df340ff528389129f68e4
Ivan Bely <belystork@gmail.com>
2021-11-26 00:13:33 +0300

    Время разбивать камни: небольшие правки

--- src/main.js
+++ src/main.js
@@ -10,26 +10,24 @@ import { createEventItemTemplate } from './view/event-item-view';
 import { createEditPointTemplate } from './view/edit-point-view';
 
 const mainControls = document.querySelector('.trip-main');
-const mainControlsNav = mainControls.querySelector('.trip-controls__navigation');
-const mainControlsFilter = mainControls.querySelector('.trip-controls__filters');
+const mainControlsNav = document.querySelector('.trip-controls__navigation');
+const mainControlsFilter = document.querySelector('.trip-controls__filters');
 
 renderTemplate(mainControls, createTripInfoTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(mainControlsNav, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(mainControlsFilter, createControlsFilterTemplate(), RenderPosition.BEFOREEND);
 
 const eventsContainer = document.querySelector('.trip-events');
-const eventsContainerTitle = eventsContainer.querySelector('h2');
 
-renderTemplate(eventsContainerTitle, createEventSorterTemplate(), RenderPosition.AFTEREND);
-
-const eventsContainerSort = eventsContainer.querySelector('.trip-sort');
-renderTemplate(eventsContainerSort, createEventListTemplate(), RenderPosition.AFTEREND);
+renderTemplate(eventsContainer, createEventSorterTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(eventsContainer, createEventListTemplate(), RenderPosition.BEFOREEND);
 
 const eventsList = document.querySelector('.trip-events__list');
-renderTemplate(eventsList, createEditPointTemplate(), RenderPosition.BEFOREEND);
 
 const EVENTS_AMOUNT = 3;
 
 for (let i = 0; i < EVENTS_AMOUNT; i++) {
   renderTemplate(eventsList, createEventItemTemplate(), RenderPosition.BEFOREEND);
 }
+
+renderTemplate(eventsList, createEditPointTemplate(), RenderPosition.AFTERBEGIN);
--- src/view/controls-filter-view.js
+++ src/view/controls-filter-view.js
@@ -6,12 +6,12 @@ export const createControlsFilterTemplate = () => (`
   </div>
 
   <div class="trip-filters__filter">
-    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future" disabled>
+    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
     <label class="trip-filters__filter-label" for="filter-future">Future</label>
   </div>
 
   <div class="trip-filters__filter">
-    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" disabled>
+    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
     <label class="trip-filters__filter-label" for="filter-past">Past</label>
   </div>
 
