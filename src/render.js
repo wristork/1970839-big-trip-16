d7ea149e2f9fa4958d49767de8fc2ca885eba208
Ivan Bely <belystork@gmail.com>
2021-11-27 17:06:41 +0300

    Время разбивать камни

--- src/render.js
+++ src/render.js
@@ -2,9 +2,9 @@ export const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
   BEFOREEND: 'beforeend',
-  AFTEREND: 'afterend' 
+  AFTEREND: 'afterend',
 };
 
 export const renderTemplate = (container, template, position = RenderPosition.BEFOREEND) => {
-  return container.insertAdjacentHTML(position, template);
-}
+  container.insertAdjacentHTML(position, template);
+};
