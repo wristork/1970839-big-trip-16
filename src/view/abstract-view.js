6d346f386444fae143b2e5a03c19642447ffc05e
Wristork <belystork@gmail.com>
2021-12-07 21:40:44 +0300

    Создан абстрактный класс для view

---
+++ src/view/abstract-view.js
@@ -0,0 +1,27 @@
+import { createElement } from "../render";
+
+export default class AbstractView {
+  #element = null;
+
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error(`Can't create instance of the abstract class: ${this.constructor.name}`);
+    }
+  }
+
+  get element() {
+    if (this.#element === null) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    throw new Error(`The getter "template" should be overwritten in inheritor class: ${this.constructor.name}`);
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
