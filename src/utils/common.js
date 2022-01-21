2fcd072b50401b2f2d382d327f2b6e00bdedf947
Wristork <belystork@gmail.com>
2022-01-21 19:26:00 +0300

    Рефакторинг getFilteredEventsByDate()

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,28 +4,16 @@ export const getFilteredEventsByDate = (events, filterType, relativeDate) => {
   switch(filterType) {
     case FilterTypes.FUTURE:
       events = events.filter((event) => {
-        const startDate = event.date.start;
         const endDate = event.date.end;
 
-        if (relativeDate - startDate <= 0 ||
-            relativeDate - startDate >= 0 && relativeDate - endDate < 0) {
-          return true;
-        }
-
-        return false;
+        return relativeDate - endDate < 0;
       });
       break;
     case FilterTypes.PAST:
       events = events.filter((event) => {
         const startDate = event.date.start;
-        const endDate = event.date.end;
 
-        if (relativeDate - endDate > 0 ||
-            relativeDate - startDate >= 0 && relativeDate - endDate < 0) {
-          return true;
-        }
-
-        return false;
+        return relativeDate - startDate > 0;
       });
       break;
   }
